04cb08d9a779fdd0475fc93cb28689e65d165a78
FabianPJimenez <fabianjosepenajimenez@gmail.com>
2023-10-26 20:42:12 -0600

    Final Examen FPeñas-BBlanco

--- proyectoFpenas-Bblanco/src/app/app-routing.module.ts
+++ proyectoFpenas-Bblanco/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './forms/forms.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'forms', component: FormsComponent},
+  {path:'', redirectTo:'/forms', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path:'', redirectTo:'/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyectoFpenas-Bblanco/src/app/app.component.ts
+++ proyectoFpenas-Bblanco/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsComponent } from './forms/forms.component';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'proyectoFpenas-Bblanco';
