96f30e46f4631f2fee0b12c35aa5c3d440161eee
BrandonZS <abrandon34zs@gmail.com>
2023-10-26 19:43:19 -0600

    final commit

--- scalderon-bzamora/src/app/app.module.ts
+++ scalderon-bzamora/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
