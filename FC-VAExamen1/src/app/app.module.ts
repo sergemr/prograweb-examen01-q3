18c2f5163d88357e9f7b05889e63298a79cd0b62
Valeria Alfaro Quesada <valfaro2409@gmail.com>
2023-10-26 19:10:55 -0600

    Examen

--- FC-VAExamen1/src/app/app.module.ts
+++ FC-VAExamen1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
    
 
   ],
