08e20ade6fd20f9fab9561c419c8f078324dfa2b
Kalfarito <keil8ralfaro2002@gmail.com>
2023-10-26 20:07:08 -0600

    Segundo Commit
    Segundo Commit Keylor Antony

---
+++ Kalfaro-Montoya/ExamenKALFARO-AMONTOYA/src/app/form/form.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import{
+  FormControl,
+  FormGroupDirective,
+  FormsModule,
+  NgForm,
+} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+  standalone: true,
+  imports:[FormsModule,MatInputModule,MatFormFieldModule]
+
+})
+
+export class FormComponent {
+formData={
+  nombre:'',
+  apellido:'',
+  correo:'',
+  edad:'',
+  numeroidentidad:'',
+};
+ 
+onSudmit(){
+  console.log(this.formData)
+  this.onReset();
+}
+onReset(){
+  this.formData={
+    nombre:'',
+  apellido:'',
+  correo:'',
+  edad:'',
+  numeroidentidad:'',
+   
+  };
+}
+}
